313baf010211321f3fe6766ee398877c48936c78
Akayy0 <joaovitorotoni6@gmail.com>
2024-02-07 17:21:44 -0300

    more routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,59 @@ import {
   createBrowserRouter,
   RouterProvider,
   Route,
+  Outlet,
 } from "react-router-dom";
+
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Write from "./pages/Write"
+import Home from "./pages/Home"
+import Single from "./pages/Single"
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return(
+    <>
+    <Navbar/>
+    <Outlet/>
+    <Footer/>
+
+    </>
+  )
+}
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Home!</div>
+    element: <Layout/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>,
+
+      },
+      {
+        path: "/post/:id",
+        element: <Single/>,
+
+      },
+      {
+        path: "/write",
+        element: <Write/>,
+
+      },
+    ]
   },
   {
-    path: "/test",
-    element: <div>Test!</div>
+    path: "/register",
+    element: <Register/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
   },
 
 ]);
@@ -25,5 +67,8 @@ function App() {
     </div>
   );
 }
+
+
+
 
 export default App;
